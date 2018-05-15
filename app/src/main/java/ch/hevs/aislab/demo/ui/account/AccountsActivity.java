d03c110af7097c7ebbdae238052aa0325f26c1f2
StefanEggenschwiler <stefan.eggenschwiler@hevs.ch>
2018-05-15 17:20:21 +0200

    - Refactoring.

--- app/src/main/java/ch/hevs/aislab/demo/ui/account/AccountsActivity.java
+++ app/src/main/java/ch/hevs/aislab/demo/ui/account/AccountsActivity.java
@@ -1,10 +1,8 @@
 package ch.hevs.aislab.demo.ui.account;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
